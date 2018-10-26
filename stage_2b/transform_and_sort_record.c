9bb7dc0c61454b394882cf4cb42bb35a5cbe0e0a
Jasmine Boyer <jasmineboy2@gmail.com>
2018-10-25 21:42:45 -0500

    finished message writing

--- stage_2b/transform_and_sort_record.c
+++ stage_2b/transform_and_sort_record.c
@@ -4,11 +4,6 @@
 #include <sys/time.h>
 #include "helper/record.h"
 
-void remove_trailing_comma(char * location);
-void transform_record(int first_record_id, int last_record_id);
-void sort_records(int first_record_id, int last_record_id);
-int compareByName(const void *a, const void *b);
-
 struct message {
     int user_id;
     char text[TEXT_LONG];
@@ -21,7 +16,11 @@ struct user {
     char location[TEXT_LONG]; /* Sorting by location */
 };
 
-
+void remove_trailing_comma(char * location);
+void transform_record(int first_record_id, int last_record_id);
+void sort_records(int first_record_id, int last_record_id);
+int compareByName(const void *a, const void *b);
+int compare_by_date(const void *a, const void *b);
 
 int main(int argc, char **argv)
 {
@@ -67,8 +66,7 @@ void transform_record(int first_record_id, int last_record_id) {
         sprintf(path, "data/user_%06d.dat", i);
         
         FILE *user_file = fopen(path, "w+");
-        
-        fprintf(user_file, "%06d\t", rp->id);
+        fprintf(user_file, "%06d\t%d\t", rp->id, rp->message_num);
         fprintf(user_file, "%s\t", rp->name);
         remove_trailing_comma(rp->location);
         fprintf(user_file, "%s\n", rp->location);
@@ -128,27 +126,23 @@ void transform_record(int first_record_id, int last_record_id) {
 /* Assuming transform_record has already been run */
 void sort_records(int first_record_id, int last_record_id) {
     char path[TEXT_SHORT];
-    char buffer[160];
-    char other[160];
     FILE *user_fp;
-    struct user users[last_record_id-first_record_id+1];
-    int i;
+    struct user *users = malloc(sizeof(struct user) * (last_record_id-first_record_id+1));
+    int i, msgs;
+    int message_number = 0; 
+    
     for (i = 0; i <= (last_record_id-first_record_id); i++) {
         sprintf(path, "data/user_%06d.dat", first_record_id+i);
         user_fp = fopen(path, "r");
-        while(fscanf(user_fp, "%d\t%[^\t]\t%[^\n]\n", &users[i].user_id, users[i].name, users[i].location) != EOF) ;
+        while(fscanf(user_fp, "%d\t%d\t%[^\t]\t%[^\n]\n", &users[i].user_id, &msgs, users[i].name, users[i].location) != EOF);
+        message_number += msgs;
         fclose(user_fp);
     }
 
-    /* enter how query should be filtered */
+    /* enter how query should be filtered compareByName or compareByLocation*/
     qsort(users, last_record_id-first_record_id+1, sizeof(struct user), compareByName);
     
-    /* FOR DEBUGGING PURPOSES */
-    printf("MAYBE SORTED NAMES: \n");
-    for (i = 0; i <= (last_record_id-first_record_id); i++) {
-        printf("Reocrd: %d\t%s\t%s\n", users[i].user_id, users[i].name, users[i].location);
-    }
-    
+    /* Rewrite users from sorted array */
     for (i = 0; i <= (last_record_id-first_record_id); i++) {
         sprintf(path, "data/user_%06d.dat", first_record_id+i);
         user_fp = fopen(path, "w");
@@ -156,7 +150,87 @@ void sort_records(int first_record_id, int last_record_id) {
         fclose(user_fp);
     }
 
+    /* declare */
+    struct message *messages = malloc(sizeof(struct message) * message_number);
+    int messages_per_file = ceil(message_number/(last_record_id-first_record_id));
+    int num_of_message_files = ceil(message_number/messages_per_file);
 
+    /* Populate message array */ 
+    int j = 0; 
+    for (i = 0; i <= num_of_message_files; i++) {
+        sprintf(path, "data/message_%06d.dat", i);
+        FILE *message_fp = fopen(path, "r");
+        while (fscanf(message_fp, "%d\t%[^\t]\t%[^\n]\n", &messages[j].user_id, messages[j].date, messages[j].text) != EOF) {
+           j++;
+           if (j >= message_number) break;
+        }
+        fclose(message_fp);
+    }
+    
+    /* Sorting Messages by Date */
+    qsort(messages, message_number, sizeof(struct message), compare_by_date);
+
+    /* Rewrite messages from sorted array */ 
+    j = 0; 
+    int k = 0;
+    printf("%d\n", num_of_message_files);
+    for (i = 0; i <= num_of_message_files; i++) {
+        sprintf(path, "data/message_%06d.dat", i);
+        FILE *message_fp = fopen(path, "w");
+        while (j < messages_per_file) {
+            if (k >= message_number) break;
+            fprintf(message_fp, "%06d\t%s\t%s\n", messages[k].user_id, messages[k].date, messages[k].text);
+            j++;
+            k++;
+        }
+        j = 0; 
+        fclose(message_fp);
+    }
+
+    /* Release Heap Memory Usage */
+    free(users);
+    free(messages);
+}
+
+int compare_by_date(const void *a, const void *b) {
+    struct message *message1 = (struct message*)a;
+    struct message *message2 = (struct message*)b;
+    int year1, year2, month1, month2, day1, day2, hour1, hour2, minutes1, minutes2;
+    char time1[TEXT_SHORT], time2[TEXT_SHORT];
+    sscanf(message1->date, "%d/%d/%d %d:%d", &month1, &day1, &year1, &hour1, &minutes1); 
+    sscanf(message2->date, "%d/%d/%d %d:%d", &month2, &day2, &year2, &hour2, &minutes2);
+
+    if (year1 > year2) {
+        return 1;
+    } else if (year2 > year1) {
+        return -1;
+    } else {
+        if (month1 > month2) {
+            return 1;
+        } else if (month2 > month1) {
+            return -1;
+        } else {
+            if (day1 > day2) {
+                return 1;
+            } else if (day2 > day1) {
+                return -1;
+            } else {
+                if (hour1 > hour2) {
+                    return 1;
+                } else if (hour2 > hour1) {
+                    return -1;
+                } else {
+                    if (minutes1 > minutes2) {
+                        return 1;
+                    } else if (minutes2 > minutes1) {
+                        return -1;
+                    } else {
+                        return 0;
+                    }
+                }
+            }
+        }
+    }
 }
 
 int compareByName(const void *a, const void *b) {
